85803cad1273d47032833a00e3ea33944a64807e
unknown <macrowell@yahoo.com>
2022-05-23 18:21:50 -0400

    completed first two tests on Episode component

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,25 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
 
-test("renders the summary test passed as prop", () => { });
+
+const testEpisode = {
+    id: 553946,
+    image: '',
+    summary: 'Placeholder summary',
+}
+
+test("renders without error", () => {
+    render(<Episode episode={testEpisode} />)
+ });
+
+test("renders the summary test passed as prop", () => {
+    render(<Episode episode={testEpisode} />)
+    const summary = screen.queryByText(/placeholder summary/i)
+
+    expect(summary).toBeInTheDocument()
+    expect(summary).not.toBeNull()
+    expect(summary).toBeTruthy()
+ });
 
 test("renders default image when image is not defined", () => { });
